89b6040f27b8e1dab6751b9687d7b85bf327b4c8
raghvind-yadav <raghvind92328@gmail.com>
2022-08-13 14:20:08 +0530

    second

--- lib/screen/headlines_screen.dart
+++ lib/screen/headlines_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_app/services/news.dart';
@@ -12,19 +11,42 @@ class Headlines extends StatefulWidget {
 }
 
 class _HeadlinesState extends State<Headlines> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // getAllNews();
+  }
 
-  Future<List<News>> getAllNews() async {
-    const url = 'https://newsapi.org/v2/everything?q=tesla&from=2022-07-12&sortBy=publishedAt&apiKey=950f172b134f472b936a966687c9cc93';
-    var response = await http.get(Uri.parse(url));
-    var jsonData = json.decode(response.body);
-    var jsonArray = jsonData['articles'];
-
-    List<News> newss = [];
-    for (var jsonNews in jsonArray){
-      News news = News(source: jsonNews['source'], author: jsonNews['author'], title: jsonNews['title'], description: jsonNews['description'], url: jsonNews['url'], urltoUmage: jsonNews['urlToImage'], publishedAt: jsonNews['publishedAt'], content: jsonNews['content']);
-      newss.add(news);
+  Future<List<News>> _getAllNews() async {
+    const url =
+        'https://newsapi.org/v2/everything?q=tesla&from=2022-07-12&sortBy=publishedAt&apiKey=950f172b134f472b936a966687c9cc93';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) 
+    {
+      var jsonData = jsonDecode(response.body);
+      // print(response.body);
+      var jsonArray = jsonData['articles'];
+      print(jsonArray);
+      print(jsonArray.length);
+      List<News> list_news = [];
+      for (var jsonNews in jsonArray) {
+        News news = News(
+            source: jsonNews['source'],
+            author: jsonNews['author'],
+            title: jsonNews['title'],
+            description: jsonNews['description'],
+            url: jsonNews['url'],
+            urltoImage: jsonNews['urlToImage'],
+            publishedAt: jsonNews['publishedAt'],
+            content: jsonNews['content']);
+        list_news.add(news);
+      }
+      // list_news = list_news.toList();
+      return list_news;
+    } else {
+      throw Exception("Failed to load News from API");
     }
-    return newss;
   }
 
   @override
@@ -36,16 +58,37 @@ class _HeadlinesState extends State<Headlines> {
           child: Text(
             'HEADLINES',
             style: TextStyle(
-              fontSize: 29,
-              color: Color(0xffffffff),
-                fontFamily: 'RobotoSlab', 
-                fontWeight: FontWeight.bold
-                ),
+                fontSize: 29,
+                color: Color(0xffffffff),
+                fontFamily: 'RobotoSlab',
+                fontWeight: FontWeight.bold),
           ),
         ),
       ),
       backgroundColor: Color(0xff464646),
-      // body: ,
+      body: FutureBuilder<List<News>>(
+        future: _getAllNews(),
+        builder: (context, snapshot) {
+          if (snapshot.data == null) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          } else {
+            List<News> news = snapshot.data!;
+            return ListView.builder(
+                itemCount: news.length,
+                itemBuilder: (context, index) {
+                  News one_news = news[index];
+                  return Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(news[index].author),
+                  );
+                });
+          }
+        },
+      ),
     );
   }
 }
--- lib/services/news.dart
+++ lib/services/news.dart
@@ -6,8 +6,20 @@ class News{
   final String title;
   final String description;
   final String url;
-  final String urltoUmage;
+  final String urltoImage;
   final String publishedAt;
   final String content;
-  News({required this.source,required this.author,required this.title,required this.description,required this.url,required this.urltoUmage,required this.publishedAt,required this.content});
+  News({required this.source,required this.author,required this.title,required this.description,required this.url,required this.urltoImage,required this.publishedAt,required this.content});
+  factory News.fromJson(Map<String,dynamic> map){
+    return News(
+    source: map['source'],
+    author: map['author'],
+    title : map['title'],
+    description: map['description'],
+    url: map['author'],
+    urltoImage: map['author'],
+    publishedAt: map['author'],
+    content: map['author']);
+  }
+
 }
